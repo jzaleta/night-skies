c1b8068ea882822162c34e85c80231876a472a18
Javier Zaleta Martínez <94091554+j-cordz@users.noreply.github.com>
2023-05-01 23:06:40 -0600

    Update sketch.js to add small secret

--- sketch.js
+++ sketch.js
@@ -115,3 +115,9 @@ class ShootingStar {
     this.y += this.yv;
   }
 }
+
+console.log(
+  "%c Created by Javier Zaleta Martínez ",
+  "color: #fff; background: hsl(0, 93%, 45%); padding: 5px 0;"
+);
+console.log();
