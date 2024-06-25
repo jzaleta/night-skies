9fb34f9a86e00b6dd7690d3e34d2c6b1a187c448
Javier Zaleta <javizaleta@proton.me>
2024-06-25 12:38:20 -0600

    feat: :mag: Add meta tags

---
+++ src/lib/components/meta.svelte
@@ -0,0 +1,27 @@
+<script>
+  let title = "Javier Zaleta"; // that's me!
+  let name = "Night Skies ✨"; // site name
+  let description =
+    "Starry-sprinkled night skies with a chance of shooting stars and lofi beats."; // page description
+  let image = "https://github.com/jzaleta.png"; // social card image URL
+
+  const makeTitle = (title, name) =>
+    title === name ? title : `${title} | ${name}`;
+</script>
+
+<svelte:head>
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content={name} />
+  <title>{makeTitle(title, name)}</title>
+  <meta property="og:title" content={makeTitle(title, name)} />
+  <meta name="twitter:title" content={makeTitle(title, name)} />
+  <meta name="description" content={description} />
+  <meta property="og:description" content={description} />
+  <meta name="twitter:description" content={description} />
+  <meta property="og:image" content={image} />
+  <meta name="twitter:image" content={image} />
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
   import About from "$lib/components/about.svelte";
   import Confetti from "$lib/components/confetti.svelte";
   import Javi from "$lib/components/javi.svelte";
+  import Meta from "$lib/components/meta.svelte";
   import Music from "$lib/components/music.svelte";
 </script>
 
 <About />
 <Confetti />
 <Javi />
+<Meta />
 <Music />
