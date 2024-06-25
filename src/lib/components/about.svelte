b372f4aa14bb97808792f8024e0fa61670911fb1
Javier Zaleta <javizaleta@proton.me>
2024-06-25 12:01:54 -0600

    feat: :sparkles: Add an about component

---
+++ src/lib/components/about.svelte
@@ -0,0 +1,22 @@
+<div class="min-h-screen flex flex-col">
+  <div class="flex-grow">
+    <!-- idk -->
+  </div>
+
+  <footer class="w-full">
+    <div class="flex flex-col items-center py-4">
+      <p class="text-center text-base">
+        A project built w/ love and &lt;\&gt;
+        <br /> by
+        <a
+          href="https://jzaleta.me"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit jzaleta's website"
+        >
+          @jzaleta
+        </a>
+      </p>
+    </div>
+  </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+  import About from "$lib/components/about.svelte";
   import Confetti from "$lib/components/confetti.svelte";
   import Javi from "$lib/components/javi.svelte";
 </script>
 
+<About />
 <Confetti />
 <Javi />
