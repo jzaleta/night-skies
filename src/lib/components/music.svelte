026f468623c9669c3cbf12eb51c401f7d9015d19
Javier Zaleta <javizaleta@proton.me>
2024-06-25 12:27:15 -0600

    feat: :alembic: Setup some lofi beats

---
+++ src/lib/components/music.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+
+  let audio;
+  let musicPlaying = false;
+
+  const toggleMusic = () => {
+    if (audio) {
+      if (musicPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      musicPlaying = !musicPlaying;
+    }
+  };
+
+  onMount(() => {
+    audio = new Audio("music/Gymnopedie.mp3");
+    audio.loop = true;
+  });
+</script>
+
+<div class="min-h-screen flex flex-col items-center">
+  <button
+    on:click={toggleMusic}
+    class="fixed top-4 left-4 px-2 py-1 text-sm font-medium text-black bg-white border border-white rounded-md cursor-pointer"
+  >
+    {#if musicPlaying}
+      Pause music 🔇
+    {:else}
+      Play music 🔊
+    {/if}
+  </button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import About from "$lib/components/about.svelte";
   import Confetti from "$lib/components/confetti.svelte";
   import Javi from "$lib/components/javi.svelte";
+  import Music from "$lib/components/music.svelte";
 </script>
 
 <About />
 <Confetti />
 <Javi />
+<Music />
