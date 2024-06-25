1a74502fb50db359296598540c7f91ca9a4a5c7c
Javier Zaleta <javizaleta@proton.me>
2024-06-24 23:13:02 -0600

    feat: :sparkles: Create and import emoji confetti component

---
+++ src/lib/components/confetti.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+  import Confetti from "js-confetti";
+
+  const spaceEmojis = [
+    "🪐",
+    "🔭",
+    "👽",
+    "🛸",
+    "✨",
+    "🌎",
+    "🌌",
+    "🌞",
+    "🌝",
+    "🚀",
+  ];
+  let confetti;
+
+  function triggerConfetti() {
+    confetti?.addConfetti({ emojis: spaceEmojis });
+  }
+
+  onMount(() => {
+    confetti = new Confetti();
+    document.addEventListener("click", triggerConfetti);
+
+    return () => {
+      document.removeEventListener("click", triggerConfetti);
+      confetti = null;
+    };
+  });
+</script>
