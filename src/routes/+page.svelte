9c4be46fc33eab276b76410388e345d6f672d3a5
Javier Zaleta <javizaleta@proton.me>
2024-06-26 11:08:12 -0600

    feat: :sparkles: Setup three.js starfield component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script>
+  import { Canvas } from "@threlte/core";
+  import Scene from "$lib/components/scene.svelte";
+
   import About from "$lib/components/about.svelte";
   import Confetti from "$lib/components/confetti.svelte";
   import Javi from "$lib/components/javi.svelte";
   import Meta from "$lib/components/meta.svelte";
   import Music from "$lib/components/music.svelte";
 </script>
+
+<main style:height="100vh" style:width="100vw">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</main>
 
 <About />
 <Confetti />
